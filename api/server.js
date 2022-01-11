854bbf1a728f8f0e5e4b1afe3b9683b677aa757c
EstrellaDionis <EstrellaDionis@gmail.com>
2022-01-10 20:49:35 -0500

    server.get for id/specific person running

--- api/server.js
+++ api/server.js
@@ -1,10 +1,34 @@
 // BUILD YOUR SERVER HERE
 const express = require('express')
-
+const User = require('./users/model') //this is going into the model.js and pulling out all the functions that are in module.exports
 const server = express()
 
 server.get('/api/users', (req, res) => {
-    res.json('users')
+    User.find()
+    .then(users => {
+        console.log(users)
+    })
+    .catch(err => {
+        res.status(500).json({
+            message: 'error getting users',
+            err: err.message,
+            stack: err.stack,
+        })
+    })
+})
+
+server.get('/api/users/:id', (req, res) => {
+    User.findById(req.params.id)
+    .then(user => {
+        res.json(user)
+    })
+    .catch(err => {
+        res.status(500).json({
+            message: 'error getting user',
+            err: err.message,
+            stack: err.stack,
+        })
+    })
 })
 
 
